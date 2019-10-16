25fa7e202f1d883f1f4760e2d05718dbae7ab3fd
zhouhuafei <zhouhuafei@wowkai.cn>
2019-10-16 21:54:30 +0800

    feat: wating...

--- index.js
+++ index.js
@@ -5,46 +5,64 @@ const path = require('path')
 const chalk = require('chalk')
 const argv = require('yargs').argv
 
-argv.entry = './dist/app.json'
-argv.output = './dist/title.json'
-
 const entry = argv.entry
 const output = argv.output
+const pages = argv.pages
+const isJs = path.extname(output) === '.js'
+
 if (!entry) {
-  console.log(chalk.red('入口路径必填'))
+  console.log(chalk.red('entry：入口路径必填'))
   return
 }
 if (!output) {
-  console.log(chalk.red('出口路径错误'))
+  console.log(chalk.red('output：出口路径错误'))
+  return
+}
+if (!pages) {
+  console.log(chalk.red('pages：页面路径错误'))
   return
 }
 
-console.log('__dirname', __dirname)
-console.log('__filename', __filename)
-console.log('process.cwd()', process.cwd())
-console.log(path.resolve('./'));
+console.log(chalk.blue(__dirname))
+console.log(chalk.blue(__filename))
+console.log(chalk.blue(process.cwd()))
+console.log(chalk.blue(path.resolve('./')))
 
-// const appJson = fs.readFileSync(entry, { encoding: 'utf-8' })
-// const objAppJson = JSON.parse(appJson)
-// const result = objAppJson.pages.map(v => {
-//   const obj = {
-//     path: v
-//   }
-//   const pageJson = fs.readFileSync(`${__dirname}/src/${v}.json`, { encoding: 'utf-8' })
-//   const objPageJson = JSON.parse(pageJson)
-//   obj.title = objPageJson.navigationBarTitleTextBak || objPageJson.navigationBarTitleText || ''
-//   return obj
-// })
-// objAppJson.subpackages.forEach(v1 => {
-//   v1.pages.forEach(v => {
-//     const path = v1.root + v
-//     const obj = { path }
-//     const pageJson = fs.readFileSync(`${__dirname}/src/${path}.json`, { encoding: 'utf-8' })
-//     const objPageJson = JSON.parse(pageJson)
-//     obj.title = objPageJson.navigationBarTitleTextBak || objPageJson.navigationBarTitleText || ''
-//     result.push(obj)
-//   })
-// })
-// fs.writeFileSync(output, JSON.stringify(result, null, '\t'), { encoding: 'utf-8' })
+console.log(chalk.cyan(path.join(process.cwd(), entry)))
+console.log(chalk.cyan(path.join(process.cwd(), output)))
+console.log(chalk.cyan(path.join(process.cwd(), pages)))
 
-console.log(chalk.green(`文件生成完毕，文件路径为：${path.join(__dirname, output)}`))
+console.log(chalk.blue(path.join('./', entry)))
+console.log(chalk.blue(path.join('./', output)))
+console.log(chalk.blue(path.join('./', pages)))
+
+console.log(chalk.cyan(path.resolve('./', entry)))
+console.log(chalk.cyan(path.resolve('./', output)))
+console.log(chalk.cyan(path.resolve('./', pages)))
+
+const appJson = fs.readFileSync(entry, { encoding: 'utf-8' })
+const objAppJson = JSON.parse(appJson)
+const result = objAppJson.pages.map(v => {
+  const obj = {
+    path: v
+  }
+  const pageJson = fs.readFileSync(path.join(pages, `${v}.json`), { encoding: 'utf-8' })
+  obj.json = JSON.parse(pageJson || '{}')
+  return obj
+})
+objAppJson.subpackages.forEach(v1 => {
+  v1.pages.forEach(v => {
+    const myPath = v1.root + v
+    const obj = { path: myPath }
+    const pageJson = fs.readFileSync(path.join(pages, `${myPath}.json`), { encoding: 'utf-8' })
+    obj.json = JSON.parse(pageJson || '{}')
+    result.push(obj)
+  })
+})
+let resultStr = JSON.stringify(result, null, '\t')
+if (isJs) {
+  resultStr = `module.exports = ${resultStr}`
+}
+fs.writeFileSync(output, resultStr, { encoding: 'utf-8' })
+
+console.log(chalk.blue(`文件生成完毕，文件路径为：${path.join(__dirname, output)}`))
